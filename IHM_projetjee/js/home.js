e25f095770e6900dca7f159c0ae9980ebed2781e
anaickgithub <anaickm@gmail.com>
2017-02-19 20:52:40 +0100

    ihm done

---
+++ IHM_projetjee/js/home.js
@@ -0,0 +1,9 @@
+function showFormAddURL(){
+	//alert("test");
+
+	var formAdd = document.getElementById("formAdd");
+	var divbtnadd = document.getElementById("divbtnadd");
+
+	formAdd.style.display = "block";
+	divbtnadd.style.display = "none";
+}
