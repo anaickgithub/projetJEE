8d117fc74dfd40f91abdde728a47ebd26b69a9b3
nl0295 <nicolas.lequin@hei.fr>
2017-02-20 02:05:10 +0100

    config+algo nouveau url

---
+++ projetjee-core/src/main/java/hei/projetJEE/config/AppConfig.java
@@ -0,0 +1,25 @@
+package hei.projetJEE.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by nicol on 20/02/2017.
+ */
+@Configuration
+@ComponentScan(basePackages = "hei.projetJEE.service")
+public class AppConfig
+{
+    @Bean
+    public Properties dbProperties() throws  IOException{
+        Properties properties = new Properties();
+        properties.load(AppConfig.class.getClassLoader().getResourceAsStream("db.properties"));
+        return properties;
+    }
+}
---
+++ projetjee-core/src/main/java/hei/projetJEE/entity/URLPerso.java
@@ -0,0 +1,86 @@
+package hei.projetJEE.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by nicol on 20/02/2017.
+ */
+
+@Entity
+public class URLPerso {
+
+    static String newURL="";
+
+    @Id
+    private String url;
+    private String urlCourt;
+    private boolean perso;
+
+    // Constructeurs
+    public URLPerso(){}
+
+    public URLPerso(String pUrl){
+        this.url=pUrl;
+    }
+
+    // Getters/Setters
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlCourt() {
+        return urlCourt;
+    }
+
+    public void setUrlCourt(String urlCourt) {
+        this.urlCourt = urlCourt;
+    }
+
+    public boolean isPerso() {
+        return perso;
+    }
+
+    public void setPerso(boolean perso) {
+        this.perso = perso;
+    }
+
+
+    //Génération urlCourt
+    public void raccourcir() {
+        String str = newURL;
+        int length=str.length();
+        byte[] byteStr = str.getBytes();
+        int up=length-1;
+
+        while(byteStr[up]>121){
+            up--;
+            if(up==-1)break;
+        }
+
+        if (up==-1)
+        {
+            str="";
+            for(int i=0;i<length+1;i++)
+                str+="a";
+        }
+        else{
+            char c=str.charAt(up);
+            c++;
+            str=str.substring(0,up)+c;
+            for(int i=up;i<length-1;i++)
+                str+="a";
+        }
+        newURL=str;
+    }
+
+
+
+
+
+
+}
