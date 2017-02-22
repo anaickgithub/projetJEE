a8b4167a4418ede0d3c6e06849a2fc32041e4f85
anaickgithub <anaickm@gmail.com>
2017-02-22 11:36:55 +0100

    xz

--- projetjee-core/src/main/java/hei/projetJEE/entity/URLPerso.java
+++ projetjee-core/src/main/java/hei/projetJEE/entity/URLPerso.java
@@ -1,9 +1,6 @@
 package hei.projetJEE.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by nicol on 10/02/2017.
